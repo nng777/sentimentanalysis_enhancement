e07a22c2bc21acbdc7e5912a9a9b649c2e06b627
nng777 <nikodemusng@gmail.com>
2025-08-24 21:31:32 +0700

    Commit

--- task.py
+++ task.py
@@ -1,6 +1,5 @@
 """
 Task
-1.Swap LSTM for GRU:
-1.1.Modify the original app.py code to replace the LSTM layer with a GRU (keras.layers.GRU) layer.
-1.2.Create Report_1.md to answer What is the final test accuracy and How does it compare to the LSTM model's performance.
+1.Modify the original app.py code to replace the LSTM layer with a GRU (keras.layers.GRU) layer.
+1.Create Report_1.md to answer What is the final test accuracy and How does it compare to the LSTM model's performance.
 """
